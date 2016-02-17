63d43452264cc78cdbe934dfeec3c42c1837b7c7
Andre Smith <andresmith@Andres-MacBook-Pro.local>
2016-02-17 14:55:12 -0500

    Adding notes to Readme.md

---
+++ README.md
@@ -0,0 +1,12 @@
+MatilaMirror Task
+
+
+* Weather Widget
+- Display 7 day forecast
+- Get widgets to auto update based on Geolocation
+
+* Traffic Widget
+- Update widget based on location
+
+* News Widget
+- show a list of new articles from CNN
--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 $(function() {
-  
-
   $('#closet-trigger').on('click', function(e) {
     e.preventDefault();
 
@@ -15,6 +13,31 @@ $(function() {
       $('.' + cont).animate({ 'opacity': '1', 'left': '0' }, 1200, 
         function() { $(this).removeClass('loaded'); });
     });
+
+
+    // Amentites
+    $("#amentites-trigger").on('click', function(e){
+      e.preventDefault();
+
+      var dest = $(this).attr('data-linkTo');
+      var cont = $(this).attr('data-container');
+
+      $('.amentites-container').load(dest, function() {
+        $('.main-container').animate({ 'opacity': '0', 'margin-left': '-100%' }, 1000,
+          function() { $(this).empty().removeClass('active-container'); });
+
+        $('' + cont).animate({ 'opacity': '1', 'left': '0' }, 1200,
+          function() { $(this).removeClass('loaded'); });
+      });
+
+
+
+    })
   });
+
+  // Amenities
+
+
+  // Future Widgets that will be loaded on this dashboard
   
 });
